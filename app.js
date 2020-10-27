f0bab0e9db21b983c268cb27675931cfbe4b82fc
APotatoChip <cvetina.a.m@gmail.com>
2020-10-27 06:51:14 +0200

    login page template/routes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const path=require('path');
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan=require('morgan');
-const connectDB=require('./config/db');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const connectDB = require('./config/db');
 
 //Load config
 dotenv.config({ path: './config/config.env' });
@@ -10,9 +12,20 @@ connectDB();
 
 const app = express();
 
-if(process.env.NODE_ENV==="development"){
+//Logging
+if (process.env.NODE_ENV === "development") {
     app.use(morgan('dev'));
 }
 
+//Handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+//Static Folder
+app.use(express.static(path.join(__dirname,'public')));
+
+//Routes
+app.use('/', require('./routes/index'));
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
