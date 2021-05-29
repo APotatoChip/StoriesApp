323253b0cb99a0e094223e9102d1e73de39e0476
APotatoChip <cvetina.a.m@gmail.com>
2021-05-29 22:35:14 +0300

    login layout, passport config, user model

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const connectDB = require('./config/db.js');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const passport = require('passport');
+const session = require('express-session');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
 
 //DB Connection 
 connectDB();
@@ -21,6 +26,17 @@ if (process.env.NODE_ENV === "development") {
 // Handlebars
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
+
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
