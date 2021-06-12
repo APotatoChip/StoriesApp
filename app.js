01d48df219d54779414bbd307cd988b3bd4bfb70
APotatoChip <cvetina.a.m@gmail.com>
2021-06-12 14:06:47 +0300

    method override for put and delete requests

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db.js');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const path = require('path');
 const passport = require('passport');
 const session = require('express-session');
@@ -20,6 +21,18 @@ require('./config/passport')(passport);
 // Body parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Method override
+app.use(
+    methodOverride(function(req, res) {
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+            // look in urlencoded POST bodies and delete it
+            let method = req.body._method
+            delete req.body._method
+            return method
+        }
+    })
+);
 
 //DB Connection 
 connectDB();
